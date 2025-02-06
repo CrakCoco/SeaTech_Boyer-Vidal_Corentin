725d42b30d9e5af22957008aa7f0d3a4894951e5
iutgeiitoulonE105 <touloniut@gmail.com>
2025-02-06 09:44:30 +0100

    update

--- Embedded/Robot_Boyer-Vidal.X/CB_TX1.c
+++ Embedded/Robot_Boyer-Vidal.X/CB_TX1.c
@@ -10,6 +10,7 @@ unsigned char isTransmitting = 0;
 void SendMessage(unsigned char* message, int length)
 {
 unsigned char i=0;
+
 if(CB_TX1_RemainingSize()>length)
 {
 //On peut écrire le message
@@ -21,33 +22,24 @@ SendOne();
 }
 void CB_TX1_Add(unsigned char value)
 {
-    // Ajout du caractère à la position du head dans le buffer
-    cbTx1Buffer[cbTx1Head] = value;
-
-    // Incrémentation du head, avec gestion du dépassement circulaire
-    cbTx1Head = (cbTx1Head + 1) % CBTX1_BUFFER_SIZE;
-
-    // Si le buffer est plein, on avance également le tail pour écraser la donnée la plus ancienne
-    if (cbTx1Head == cbTx1Tail)
+    cbTx1Buffer[cbTx1Head]=value;
+    cbTx1Head++;
+    if (cbTx1Head>=CBTX1_BUFFER_SIZE)
     {
-        // Le buffer est plein, on écrase la donnée la plus ancienne
-        cbTx1Tail = (cbTx1Tail + 1) % CBTX1_BUFFER_SIZE;
-    }
+        cbTx1Head=0;
+    } 
 }
-
-
 unsigned char CB_TX1_Get(void)
 {
-    // Récupération de la donnée à la position du tail
-    unsigned char value = cbTx1Buffer[cbTx1Tail];
-
-    // Incrémentation du tail avec gestion du dépassement circulaire
-    cbTx1Tail = (cbTx1Tail + 1) % CBTX1_BUFFER_SIZE;
-
-    return value;
+    unsigned char value;
+    value=cbTx1Buffer[cbTx1Tail];
+    cbTx1Tail++;
+    if (cbTx1Tail>=CBTX1_BUFFER_SIZE)
+    {
+        cbTx1Tail=0;
+    }
+    return value ;
 }
-
-
 void __attribute__((interrupt, no_auto_psv)) _U1TXInterrupt(void) {
 IFS0bits.U1TXIF = 0; // clear TX interrupt flag
 if (cbTx1Tail!=cbTx1Head)
@@ -63,28 +55,33 @@ isTransmitting = 1;
 unsigned char value=CB_TX1_Get();
 U1TXREG = value; // Transmit one character
 }
-unsigned char CB_TX1_IsTranmitting(void)
-{
+unsigned char CB_TX1_IsTranmitting(void){
     return isTransmitting;
 }
-
 int CB_TX1_GetDataSize(void)
 {
-    int dataSize;
-
-    // Calcul de la taille des données dans le buffer (différence entre head et tail)
-    dataSize = (cbTx1Head - cbTx1Tail + CBTX1_BUFFER_SIZE) % CBTX1_BUFFER_SIZE;
-
-    return dataSize;
+//return size of data stored in circular buffer
+int dataSize;
+if (cbTx1Head>cbTx1Tail)
+{
+    dataSize=cbTx1Head-cbTx1Tail;
 }
-
+else
+    dataSize=CBTX1_BUFFER_SIZE-(cbTx1Tail-cbTx1Head);
+return dataSize;
+}
 int CB_TX1_GetRemainingSize(void)
 {
-    int remainingSize;
-
-    // Calcul de la taille restante dans le buffer
-    remainingSize = (CBTX1_BUFFER_SIZE - 1) - CB_TX1_GetDataSize();
-
-    return remainingSize;
+//return size of remaining size in circular buffer
+int remainingSize;
+if (cbTx1Head>cbTx1Tail)
+{
+    remainingSize =CBTX1_BUFFER_SIZE-(cbTx1Head-cbTx1Tail);
 }
-
+else
+    remainingSize=cbTx1Tail-cbTx1Head;
+return remainingSize;
+}
+int CB_TX1_RemainingSize(void){
+    return CBTX1_BUFFER_SIZE-CB_TX1_GetDataSize();
+}
